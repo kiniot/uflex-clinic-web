67dcbd2d1e6de19e16b8557d74b4efa79c77e399
Salim Ramirez <salimramirezm@gmail.com>
2026-04-28 10:55:47 -0500

    feat(app): enhance application configuration with routing, translation, and layout components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/presentation/views/home/home';
+import { iamGuard } from './iam/infrastructure/iam.guard';
 
-export const routes: Routes = [];
+const about = () => import('./shared/presentation/views/about/about').then((m) => m.About);
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
+const iamRoutes = () => import('./iam/presentation/iam.routes').then((m) => m.iamRoutes);
+
+const baseTitle = 'KinIoT - uFlex';
+
+/**
+ * Main routes configuration for the application in the presentation layer.
+ */
+export const routes: Routes = [
+  { path: 'home', component: Home, title: `${baseTitle} - Home`, canActivate: [iamGuard] },
+  { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
+  { path: 'iam', loadChildren: iamRoutes },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+];
